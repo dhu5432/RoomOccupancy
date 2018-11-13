69828620d731ac0955848107abc6b51e2bda4d6d
Daniel <dhu5432@gmail.com>
2018-11-13 16:23:22 -0500

    Throwing out features?

--- read_data.py
+++ read_data.py
@@ -9,6 +9,11 @@ def read_data():
 	features_dataframe = pd.read_table('Data/datatraining.txt', sep=",",header=None, usecols=[1,2,3,4,5,6], skiprows=[0])
 	features_dataframe.columns = ["Date", "Temperature", "Humidity", "Light", "CO2", "HumidityRatio"]
 
+	features_dataframe = pd.read_table('Data/datatraining.txt', sep=",",header=None, usecols=[3,4,5,6], skiprows=[0])
+	features_dataframe.columns = ["Humidity", "Light", "CO2", "HumidityRatio"]
+
+
+
 	features_matrix = features_dataframe.values
 
 
@@ -16,7 +21,7 @@ def read_data():
 
 	labels_dataframe.columns = ["occupancy"]
 	labels_matrix = labels_dataframe.values
-
+	'''
 	index = 0
 	for date_time in features_matrix:
 		splitted = date_time[0].split()
@@ -25,7 +30,7 @@ def read_data():
 		date_time = x.seconds
 		features_matrix[index][0] = date_time
 		index+=1
-	
+	'''	
 	fx,fy = features_matrix.shape
 	labels_matrix[labels_matrix == 0] = -1
 	
