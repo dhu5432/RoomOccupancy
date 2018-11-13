071291aee920be9be2600d2852b94a0e2c5f2a21
Daniel <dhu5432@gmail.com>
2018-11-13 14:20:48 -0500

    Kernelized Perceptron

--- main.py
+++ main.py
@@ -6,28 +6,35 @@ import datetime
 import linear_primalsvm
 import linpred
 import read_data
-def main():
+def main():	
+	iterations1=10000
 	labels_matrix, features_matrix = read_data.read_data()
-	print(labels_matrix[::500])
+	#print(labels_matrix[::500])
 	#theta = linear_perceptron.run(10000, features_matrix[0:500], labels_matrix[0:500])
-	theta = linear_perceptron.run(10000, features_matrix[::2],labels_matrix[::2])
-	print theta
+	theta = linear_perceptron.run(iterations1, features_matrix,labels_matrix)
 	count = 0
-	for i in range(1,len(features_matrix),2):
+	for i in range(0,len(features_matrix)):
 		xii = np.matrix(features_matrix[i])
 		ai = linpred.run(xii,theta)
 		bi = labels_matrix[i]
 		if(ai != bi):
-		    print('mistake')
-		    count+=1
-	print(count,'  = mistakes made')
+			#print('mistake')
+			count+=1
+	#print(count,"  = mistakes made on "+str(iterations1)+" iterations")
 	#print linear_perceptron.run(1000, features_matrix, labels_matrix)
 	#print linear_perceptron.run(10000, features_matrix[::5],labels_matrix[::5])
 	#print linear_primalsvm.run(features_matrix, labels_matrix)
 
+		
+	#file = open("results.txt", "a")
+	print("{0} mistakes made on {1} iterations on {2} data points\n". format(count, str(iterations1),sum(1 for line in open('Data/datatraining.txt'))-1))
+	#file.close()
 
 
-
+	#file = open("results10000.txt", "a")
+	#file.write("\n")
+	#file.close()
+	
 
 
 
--- read_data.py
+++ read_data.py
@@ -6,13 +6,13 @@ import linear_primalsvm
 import linpred
 
 def read_data():
-	features_dataframe = pd.read_table('Data/datatest.txt', sep=",",header=None, usecols=[1,2,3,4,5,6], skiprows=[0])
+	features_dataframe = pd.read_table('Data/datatraining.txt', sep=",",header=None, usecols=[1,2,3,4,5,6], skiprows=[0])
 	features_dataframe.columns = ["Date", "Temperature", "Humidity", "Light", "CO2", "HumidityRatio"]
 
 	features_matrix = features_dataframe.values
 
 
-	labels_dataframe = pd.read_table('Data/datatest.txt', sep=",", header=None, usecols=[7], skiprows=[0])
+	labels_dataframe = pd.read_table('Data/datatraining.txt', sep=",", header=None, usecols=[7], skiprows=[0])
 
 	labels_dataframe.columns = ["occupancy"]
 	labels_matrix = labels_dataframe.values
