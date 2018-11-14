7e641d8c46e6d48b5a15dbc9d02b27261480d2df
Daniel <dhu5432@gmail.com>
2018-11-13 20:26:36 -0500

    Using logistic regression instead of perceptron

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ import linear_primalsvm
 import linpred
 import read_data
 def main():	
-	iterations1=10000
-	labels_matrix, features_matrix = read_data.read_data()
-	#print(labels_matrix[::500])
-	#theta = linear_perceptron.run(10000, features_matrix[0:500], labels_matrix[0:500])
-	theta = linear_perceptron.run(iterations1, features_matrix,labels_matrix)
-	count = 0
-	for i in range(0,len(features_matrix)):
-		xii = np.matrix(features_matrix[i])
-		ai = linpred.run(xii,theta)
-		bi = labels_matrix[i]
-		if(ai != bi):
+	for a in range(0,4):
+		iterations1=10000
+		labels_matrix, features_matrix = read_data.read_data()
+		#print(labels_matrix[::500])
+		#theta = linear_perceptron.run(10000, features_matrix[0:500], labels_matrix[0:500])
+		theta = linear_perceptron.run(iterations1, features_matrix,labels_matrix)
+		count = 0
+		for i in range(0,len(features_matrix)):
+			xii = np.matrix(features_matrix[i])
+			ai = linpred.run(xii,theta)
+			bi = labels_matrix[i]
+			if(ai != bi):
 			#print('mistake')
-			count+=1
+				count+=1
 	#print(count,"  = mistakes made on "+str(iterations1)+" iterations")
 	#print linear_perceptron.run(1000, features_matrix, labels_matrix)
 	#print linear_perceptron.run(10000, features_matrix[::5],labels_matrix[::5])
@@ -27,7 +28,7 @@ def main():
 
 		
 	#file = open("results.txt", "a")
-	print("{0} mistakes made on {1} iterations on {2} data points\n". format(count, str(iterations1),sum(1 for line in open('Data/datatraining.txt'))-1))
+		print("{0} mistakes made on {1} iterations on {2} data points\n". format(count, str(iterations1),sum(1 for line in open('Data/datatraining.txt'))-1))
 	#file.close()
 
 
--- read_data.py
+++ read_data.py
@@ -5,12 +5,10 @@ import datetime
 import linear_primalsvm
 import linpred
 
-def read_data():
-	features_dataframe = pd.read_table('Data/datatraining.txt', sep=",",header=None, usecols=[1,2,3,4,5,6], skiprows=[0])
-	features_dataframe.columns = ["Date", "Temperature", "Humidity", "Light", "CO2", "HumidityRatio"]
+def read_training_data():
 
-	features_dataframe = pd.read_table('Data/datatraining.txt', sep=",",header=None, usecols=[3,4,5,6], skiprows=[0])
-	features_dataframe.columns = ["Humidity", "Light", "CO2", "HumidityRatio"]
+	features_dataframe = pd.read_table('Data/datatraining.txt', sep=",",header=None, usecols=[2,3,4,5,6], skiprows=[0])
+	features_dataframe.columns = ["Temperatures", "Humidity", "Light", "CO2", "HumidityRatio"]
 
 
 
@@ -34,7 +32,65 @@ def read_data():
 	fx,fy = features_matrix.shape
 	labels_matrix[labels_matrix == 0] = -1
 	
-	return labels_matrix, features_matrix
+	return features_matrix, labels_matrix
+
+def read_testing_data2():
+
+	features_dataframe = pd.read_table('Data/datatest2.txt', sep=",",header=None, usecols=[2,3,4,5,6], skiprows=[0])
+	features_dataframe.columns = ["Temperatures", "Humidity", "Light", "CO2", "HumidityRatio"]
+
+
+
+	features_matrix = features_dataframe.values
+
+
+	labels_dataframe = pd.read_table('Data/datatest2.txt', sep=",", header=None, usecols=[7], skiprows=[0])
+
+	labels_dataframe.columns = ["occupancy"]
+	labels_matrix = labels_dataframe.values
+	'''
+	index = 0
+	for date_time in features_matrix:
+		splitted = date_time[0].split()
+		[hours, minutes, seconds] = [int(x) for x in splitted[1].split(':')]
+		x = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+		date_time = x.seconds
+		features_matrix[index][0] = date_time
+		index+=1
+	'''	
+	fx,fy = features_matrix.shape
+	labels_matrix[labels_matrix == 0] = -1
+	
+	return features_matrix, labels_matrix
+
+def read_testing_data1():
+
+	features_dataframe = pd.read_table('Data/datatest.txt', sep=",",header=None, usecols=[2,3,4,5,6], skiprows=[0])
+	features_dataframe.columns = ["Temperatures", "Humidity", "Light", "CO2", "HumidityRatio"]
+
+
+
+	features_matrix = features_dataframe.values
+
+
+	labels_dataframe = pd.read_table('Data/datatest.txt', sep=",", header=None, usecols=[7], skiprows=[0])
+
+	labels_dataframe.columns = ["occupancy"]
+	labels_matrix = labels_dataframe.values
+	'''
+	index = 0
+	for date_time in features_matrix:
+		splitted = date_time[0].split()
+		[hours, minutes, seconds] = [int(x) for x in splitted[1].split(':')]
+		x = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+		date_time = x.seconds
+		features_matrix[index][0] = date_time
+		index+=1
+	'''	
+	fx,fy = features_matrix.shape
+	labels_matrix[labels_matrix == 0] = -1
+	
+	return features_matrix, labels_matrix
 
 
 	
