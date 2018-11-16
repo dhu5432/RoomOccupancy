765d3f240cd8d6e0674af9994d32d2bfbf371fa1
Daniel <dhu5432@gmail.com>
2018-11-15 21:10:02 -0500

    Changing main.py to reflect project report

--- main.py
+++ main.py
@@ -8,58 +8,58 @@ import linpred
 import kerperceptron
 import kerpred
 import read_data
-<<<<<<< HEAD
 def main():
-	labels_matrix, features_matrix = read_data.read_data()
-	#print(labels_matrix[::500])
-	#theta = linear_perceptron.run(10000, features_matrix[0:500], labels_matrix[0:500])
-	#theta = linear_perceptron.run(10000, features_matrix,labels_matrix)
-	theta = kerperceptron.run(10, features_matrix[::800],labels_matrix[::800])
-	print theta
-	print "----------"
-	count = 0
-	for i in range(0,len(features_matrix[::800])):
-		xii = np.matrix(features_matrix[i])
-		ai = kerpred.run(theta,features_matrix[::800], labels_matrix[::800],xii)
-		#ai = linpred.run(xii,theta)
-		#bi = labels_matrix[i]
-		#if(ai != bi):
-		 #   print('mistake')
-		  #  count+=1
-	print(count,'  = mistakes made')
-=======
-def main():	
-	for a in range(0,4):
-		iterations1=10000
-		labels_matrix, features_matrix = read_data.read_data()
-		#print(labels_matrix[::500])
-		#theta = linear_perceptron.run(10000, features_matrix[0:500], labels_matrix[0:500])
-		theta = linear_perceptron.run(iterations1, features_matrix,labels_matrix)
-		count = 0
-		for i in range(0,len(features_matrix)):
-			xii = np.matrix(features_matrix[i])
-			ai = linpred.run(xii,theta)
-			bi = labels_matrix[i]
-			if(ai != bi):
-			#print('mistake')
-				count+=1
-	#print(count,"  = mistakes made on "+str(iterations1)+" iterations")
->>>>>>> 996012694c7bcd1c5f39b2b5d848f2071cabb2c3
-	#print linear_perceptron.run(1000, features_matrix, labels_matrix)
-	#print linear_perceptron.run(10000, features_matrix[::5],labels_matrix[::5])
-	#print linear_primalsvm.run(features_matrix, labels_matrix)
-
 		
-	#file = open("results.txt", "a")
-		print("{0} mistakes made on {1} iterations on {2} data points\n". format(count, str(iterations1),sum(1 for line in open('Data/datatraining.txt'))-1))
-	#file.close()
-
-
-	#file = open("results10000.txt", "a")
-	#file.write("\n")
-	#file.close()
+	#Reading the data from the training set with all features included
+	features_matrix_all, labels_matrix_all = read_data.read_training_data_all()
 	
+	#Running our linear perceptron on 10,000 iterations
+	theta = linear_perceptron.run(10000, features_matrix_all, labels_matrix_all)
+	
+	#Testing convergence by seeing if the perceptron will make any mistakes predicting on the same data it trained on
+	mistakes_count = 0
+	for i in range(0, len(features_matrix_all)):
+		xii = np.matrix(features_matrix_all[i])
+		ai = linpred.run(xii, theta)
+		bi = labels_matrix_all[i]
+		if(ai != bi):
+			mistakes_count+=0
+	
+	print("{0} mistakes made on {1} iterations on {2} data points in datatraining.txt with all features included\n". format(count, str(iterations1),sum(1 for line in open('Data/datatraining.txt'))-1))
 
+	
+	
+	#Running our linear perceptron on 100,000 iterations
+	theta = linear_perceptron.run(100000, features_matrix_all, labels_matrix_all)
+	
+	#Testing convergence by seeing if the perceptron will make any mistakes predicting on the same data it trained on
+	mistakes_count = 0
+	for i in range(0, len(features_matrix_all)):
+		xii = np.matrix(features_matrix_all[i])
+		ai = linpred.run(xii, theta)
+		bi = labels_matrix_all[i]
+		if(ai != bi):
+			mistakes_count+=0
+	
+	print("{0} mistakes made on {1} iterations on {2} data points in datatraining.txt with all features included\n". format(count, str(iterations1),sum(1 for line in open('Data/datatraining.txt'))-1))
+
+
+	#Reading the data from the training set with all features included
+	features_matrix_no_date, labels_matrix_no_date = read_data.read_training_no_date()
+	
+	#Running our linear perceptron on 10,000 iterations
+	theta = linear_perceptron.run(10000, features_matrix_no_date, labels_matrix_no_date)
+	
+	#Testing convergence by seeing if the perceptron will make any mistakes predicting on the same data it trained on
+	mistakes_count = 0
+	for i in range(0, len(features_matrix_no_date)):
+		xii = np.matrix(features_matrix_no_date[i])
+		ai = linpred.run(xii, theta)
+		bi = labels_matrix_no_date[i]
+		if(ai != bi):
+			mistakes_count+=0
+	
+	print("{0} mistakes made on {1} iterations on {2} data points in datatraining.txt with all features included\n". format(count, str(iterations1),sum(1 for line in open('Data/datatraining.txt'))-1))
 
 
 
--- read_data.py
+++ read_data.py
@@ -4,19 +4,40 @@ import numpy as np
 import datetime
 import linear_primalsvm
 import linpred
+def read_training_data_all():
 
-<<<<<<< HEAD
-def read_data():
 	features_dataframe = pd.read_table('Data/datatraining.txt', sep=",",header=None, usecols=[1,2,3,4,5,6], skiprows=[0])
-	features_dataframe.columns = ["Date", "Temperature", "Humidity", "Light", "CO2", "HumidityRatio"]
-=======
-def read_training_data():
+	features_dataframe.columns = ["date/time", "Temperatures", "Humidity", "Light", "CO2", "HumidityRatio"]
+
+
+	features_matrix = features_dataframe.values
+
+
+	labels_dataframe = pd.read_table('Data/datatraining.txt', sep=",", header=None, usecols=[7], skiprows=[0])
+
+	labels_dataframe.columns = ["occupancy"]
+	labels_matrix = labels_dataframe.values
+
+	#Converting the date time feature from a string into an integer	
+	index = 0
+	for date_time in features_matrix:
+		splitted = date_time[0].split()
+		[hours, minutes, seconds] = [int(x) for x in splitted[1].split(':')]
+		x = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+		date_time = x.seconds
+		features_matrix[index][0] = date_time
+		index+=1
+	
+	fx,fy = features_matrix.shape
+	labels_matrix[labels_matrix == 0] = -1
+	
+	return features_matrix, labels_matrix
+
+def read_training_data_no_date():
 
 	features_dataframe = pd.read_table('Data/datatraining.txt', sep=",",header=None, usecols=[2,3,4,5,6], skiprows=[0])
 	features_dataframe.columns = ["Temperatures", "Humidity", "Light", "CO2", "HumidityRatio"]
 
-
->>>>>>> 996012694c7bcd1c5f39b2b5d848f2071cabb2c3
 
 	features_matrix = features_dataframe.values
 
