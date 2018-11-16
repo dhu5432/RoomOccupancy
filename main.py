c31d8534fc61f3c325504f8bb52deceb85581fd3
Jessica <@purdue.edu>
2018-11-15 19:40:42 -0500

    k fold thing done

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ import linpred
 import kerperceptron
 import kerpred
 import read_data
+import k_foldcv
+
+
 def main():	
 	for a in range(0,1):
 		iterations1=10000
 		features_matrix, labels_matrix = read_data.read_training_data()
 		#print(labels_matrix[::500])
+		
 		theta = linear_perceptron.run(10, features_matrix[0:500], labels_matrix[0:500])
+		
+		z1 = k_foldcv.run(10, features_matrix[0:500], labels_matrix[0:500])
+
 		#theta = linear_perceptron.run(iterations1, features_matrix,labels_matrix)
 		count = 0
 		for i in range(0,len(features_matrix)):
@@ -27,7 +34,7 @@ def main():
 	#print linear_perceptron.run(10000, features_matrix[::5],labels_matrix[::5])
 	#print linear_primalsvm.run(features_matrix, labels_matrix)
 
-		
+		print(z1)
 	#file = open("results.txt", "a")
 		print("{0} mistakes made on {1} iterations on {2} data points\n". format(count, str(iterations1),sum(1 for line in open('Data/datatraining.txt'))-1))
 	#file.close()
