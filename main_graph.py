30b7d90af2b4431a54743a5f088f52066d37c44a
niralirai <niralirai@yahoo.com>
2018-11-30 17:38:17 -0500

    just mean testing

--- main_graph.py
+++ main_graph.py
@@ -5,22 +5,24 @@ import datetime
 import read_data
 import k_fold_cross_validation
 import rbf_svm
+import matplotlib.pyplot as pp
+
 def main():
 	
 
 	X, y = read_data.read_training_data_all()
 
 	m = np.mean(X, axis=0)
-	s = np.std(X, axis=0)
+	#s = np.std(X, axis=0)
 	pp.figure()
 	pp.plot(m, 'b+') # b for blue, + for cross
 	pp.xlabel('Feature')
 	pp.ylabel('Mean')
 	pp.figure()
 	pp.plot(s, 'ro') # r for red, o for circle
-	pp.xlabel('Feature')
-	pp.ylabel('Standard deviation')
-	pp.show() # This command will show the two figures, and wait
+	#pp.xlabel('Feature')
+	#pp.ylabel('Standard deviation')
+	#pp.show() # This command will show the two figures, and wait
   
 
 
