314560e6cd296edd309e2c3ed083324f14c98f25
niralirai <niralirai@yahoo.com>
2018-12-01 16:10:30 -0500

    titles to graphs

--- main_graph.py
+++ main_graph.py
@@ -16,6 +16,7 @@ def run():
 	s = np.std(X1, axis=0)
 	pp.figure()
 	pp.xticks(np.arange(6), ('Time', 'Temperatures', 'Humidity', 'Light', 'CO2', 'HumidityRatio'))
+	pp.title('Mean vs. Feature')
 	pp.plot(m, 'b+') # b for blue, + for cross
 	pp.xlabel('Feature')
 	pp.ylabel('Mean')
@@ -23,19 +24,10 @@ def run():
 	pp.xticks(np.arange(5), ('Temperatures', 'Humidity', 'Light', 'CO2', 'HumidityRatio'))
 	pp.plot(s, 'ro') # r for red, o for circle
 	pp.xlabel('Feature')
+	pp.title('Standard Deviation vs. Feature')
 	pp.ylabel('Standard deviation')
 	#pp.show() # This command will show the two figures, and wait
 	
-
-	#theta = rbf_svm.train(10,X,y)
-	#print("----")
-	#print(theta)
-	#print("----")
-	#pp.figure()
-	#pp.plot(theta, 'b.') # b for blue, . for dot
-	#pp.xlabel('Feature')
-	#pp.ylabel('Theta')
- 	#pp.show() # This command will show the figure, and wait 
 	
 	
 
@@ -49,6 +41,7 @@ def run():
 		for j in range(i,6):
 			if i != j:
 				pp.figure()
+				pp.title('Feature vs. Feature: ' + str(names[i]) + ' vs. ' + str(names[j]))
 				pp.plot(X[positive_samples,i], X[positive_samples,j], 'bo') # b for blue, o for circle
 				pp.plot(X[negative_samples,i], X[negative_samples,j], 'ro') # r for red, o for circle
 				pp.xlabel('Feature: '+str(names[i]))
