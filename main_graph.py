d8049da606b36ed50310483966daf688dca78832
niralirai <niralirai@yahoo.com>
2018-11-30 22:28:15 -0500

    feature against feature

--- main_graph.py
+++ main_graph.py
@@ -11,27 +11,48 @@ def main():
 	
 
 	X, y = read_data.read_training_data_no_time()
-	"""
+	"""	
 	m = np.mean(X, axis=0)
-	#s = np.std(X, axis=0)
+	s = np.std(X, axis=0)
 	pp.figure()
+	pp.xticks([0,1, 2, 3,4])
 	pp.plot(m, 'b+') # b for blue, + for cross
 	pp.xlabel('Feature')
 	pp.ylabel('Mean')
 	pp.figure()
-	#pp.plot(s, 'ro') # r for red, o for circle
-	#pp.xlabel('Feature')
-	#pp.ylabel('Standard deviation')
-	pp.show() # This command will show the two figures, and wait
-	"""
-
-	theta = rbf_svm.train(10,X,y)
-	pp.figure()
-	pp.plot(theta, 'b.') # b for blue, . for dot
+	pp.xticks([0,1, 2, 3,4])
+	pp.plot(s, 'ro') # r for red, o for circle
 	pp.xlabel('Feature')
-	pp.ylabel('Theta')
- 	pp.show() # This command will show the figure, and wait 
+	pp.ylabel('Standard deviation')
+	pp.show() # This command will show the two figures, and wait
 	
+
+	#theta = rbf_svm.train(10,X,y)
+	#print("----")
+	#print(theta)
+	#print("----")
+	#pp.figure()
+	#pp.plot(theta, 'b.') # b for blue, . for dot
+	#pp.xlabel('Feature')
+	#pp.ylabel('Theta')
+ 	#pp.show() # This command will show the figure, and wait 
+	
+	"""
+	
+	positive_samples = list(np.where(y==1)[0])
+	negative_samples = list(np.where(y==-1)[0])
+	
+
+	for i in range(0,5):
+		for j in range(i,5):
+			if i != j:
+				pp.figure()
+				pp.plot(X[positive_samples,i], X[positive_samples,j], 'bo') # b for blue, o for circle
+				pp.plot(X[negative_samples,i], X[negative_samples,j], 'ro') # r for red, o for circle
+				pp.xlabel('Feature '+str(i))
+				pp.ylabel('Feature '+str(j))
+				#pp.show() # This command will open the figure, and wait
+ 	pp.show()
 
 
 
